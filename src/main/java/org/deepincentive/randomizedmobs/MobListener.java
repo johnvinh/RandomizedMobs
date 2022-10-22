7cdae2125dcc7010415f63cff116c5b52aa19b18
John Vinh <john.michael.vinh2@gmail.com>
2022-10-22 19:43:21 -0400

    Change package name to reflect new Minecraft username

--- src/main/java/org/deepincentive/randomizedmobs/MobListener.java
+++ src/main/java/org/deepincentive/randomizedmobs/MobListener.java
@@ -1,4 +1,4 @@
-package org.suddenclouds.randomizedmobs;
+package org.deepincentive.randomizedmobs;
 
 import org.bukkit.Material;
 import org.bukkit.entity.EntityType;
--- src/main/java/org/deepincentive/randomizedmobs/RandomizedMobs.java
+++ src/main/java/org/deepincentive/randomizedmobs/RandomizedMobs.java
@@ -1,4 +1,4 @@
-package org.suddenclouds.randomizedmobs;
+package org.deepincentive.randomizedmobs;
 
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
